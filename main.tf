154ebf206cd8c6e686ba25c3b1a0c11efe170604
binuverghese <92746182+binuverghese@users.noreply.github.com>
2025-03-13 23:10:38 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,161 +1,166 @@
 terraform {
+  required_version = ">= 1.9, < 2.0"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">=3.0.0"
+      version = "~> 3.74"
     }
-  }
-   backend "azurerm" {
-    resource_group_name  = “tfstate-bvdemo-rg”
-    storage_account_name = "tfstatebvdemo"
-    container_name       = "tfstate"
-    key                  = "tfdemo.tfstate"
+    http = {
+      source  = "hashicorp/http"
+      version = "~> 3.4"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5"
+    }
   }
 }
 
 provider "azurerm" {
-  features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
 }
 
-# Resource Group
-module "resource_group" {
-  source  = "Azure/avm-resource-group/azurerm"
-  version = "1.0.0"
+module "regions" {
+  source  = "Azure/regions/azurerm"
+  version = "~> 0.3"
+}
 
+resource "random_integer" "region_index" {
+  max = length(module.regions.regions) - 1
+  min = 0
+}
+
+module "naming" {
+  source  = "Azure/naming/azurerm"
+  version = "~> 0.3"
+}
+
+resource "azurerm_resource_group" "this" {
+  location = var.location
+  name     = var.resource_group_name
+}
+
+resource "azurerm_route_table" "this" {
+  location            = var.location
+  name                = module.naming.route_table.name_unique
   resource_group_name = var.resource_group_name
-  location           = var.location
 }
 
-# Virtual Network
-module "network" {
-  source  = "Azure/avm-networking/azurerm"
-  version = "1.0.0"
-
-  vnet_name           = var.vnet_name
-  resource_group_name = module.resource_group.name
-  location           = module.resource_group.location
-  address_space      = var.address_space
-  subnets           = var.subnets
+resource "azurerm_network_ddos_protection_plan" "this" {
+  location            = var.location
+  name                = module.naming.network_ddos_protection_plan.name_unique
+  resource_group_name = var.resource_group_name
 }
 
-# Decentralized Azure Firewall
-module "firewall" {
-  source  = "Azure/avm-firewall/azurerm"
-  version = "1.0.0"
+resource "azurerm_nat_gateway" "this" {
+  location            = var.location
+  name                = module.naming.nat_gateway.name_unique
+  resource_group_name = var.resource_group_name
+}
 
-  firewall_name       = var.firewall_name
-  resource_group_name = module.resource_group.name
-  location           = module.resource_group.location
-  vnet_name          = module.network.vnet_name
-  subnet_name        = module.network.subnets["subnet-fw"].name
+data "http" "public_ip" {
+  method = "GET"
+  url    = "http://api.ipify.org?format=json"
+}
 
-  public_ip = {
-    name              = "pip-firewall"
-    allocation_method = "Static"
+resource "azurerm_network_security_group" "https" {
+  location            = var.location
+  name                = module.naming.network_security_group.name_unique
+  resource_group_name = var.resource_group_name
+
+  security_rule {
+    access                     = "Allow"
+    destination_address_prefix = "*"
+    destination_port_range     = "443"
+    direction                  = "Inbound"
+    name                       = "AllowInboundHTTPS"
+    priority                   = 100
+    protocol                   = "Tcp"
+    source_address_prefix      = jsondecode(data.http.public_ip.response_body).ip
+    source_port_range          = "*"
   }
 }
 
-# Bastion for Remote Management
-module "bastion" {
-  source  = "Azure/avm-bastion/azurerm"
-  version = "1.0.0"
+resource "azurerm_user_assigned_identity" "this" {
+  location            = var.location
+  name                = module.naming.user_assigned_identity.name_unique
+  resource_group_name = var.resource_group_name
+}
 
-  bastion_name       = var.bastion_name
-  resource_group_name = module.resource_group.name
-  location           = module.resource_group.location
-  vnet_name         = module.network.vnet_name
-  subnet_name       = module.network.subnets["subnet-app"].name
+resource "azurerm_storage_account" "this" {
+  account_replication_type = "ZRS"
+  account_tier             = "Standard"
+  location                 = var.location
+  name                     = module.naming.storage_account.name_unique
+  resource_group_name      = var.resource_group_name
+}
 
-  public_ip = {
-    name              = "pip-bastion"
-    allocation_method = "Static"
+resource "azurerm_log_analytics_workspace" "this" {
+  location            = var.location
+  name                = module.naming.log_analytics_workspace.name_unique
+  resource_group_name = var.resource_group_name
+}
+
+module "vnet1" {
+  source              = "../../"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  name                = module.naming.virtual_network.name_unique
+  address_space       = var.address_space_vnet1
+
+  dns_servers = {
+    dns_servers = var.dns_servers
   }
-}
 
-# Application Gateway
-module "application_gateway" {
-  source  = "Azure/avm-application-gateway/azurerm"
-  version = "1.0.0"
-
-  application_gateway_name = var.application_gateway_name
-  resource_group_name      = module.resource_group.name
-  location                = module.resource_group.location
-  vnet_name               = module.network.vnet_name
-  subnet_name             = module.network.subnets["subnet-app"].name
-
-  sku = {
-    name  = "WAF_v2"
-    tier  = "WAF_v2"
-    capacity = 2
+  ddos_protection_plan = {
+    id     = azurerm_network_ddos_protection_plan.this.id
+    enable = var.enable_ddos_protection
   }
+
+  enable_vm_protection = var.enable_vm_protection
+
+  encryption = {
+    enabled     = var.encryption_enabled
+    enforcement = var.encryption_enforcement
+  }
+
+  flow_timeout_in_minutes = var.flow_timeout
 }
 
-# Private DNS (Decentralized)
-module "private_dns" {
-  source  = "Azure/avm-private-dns/azurerm"
-  version = "1.0.0"
+module "vnet2" {
+  source              = "../../"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  name                = "${module.naming.virtual_network.name_unique}2"
+  address_space       = var.address_space_vnet2
 
-  dns_zone_name      = var.dns_zone_name
-  resource_group_name = module.resource_group.name
-  vnet_name         = module.network.vnet_name
-}
+  encryption = {
+    enabled     = var.encryption_enabled
+    enforcement = var.encryption_enforcement
+  }
 
-# NSG with Security Baseline
-module "network_security_group" {
-  source  = "Azure/avm-nsg/azurerm"
-  version = "1.0.0"
-
-  nsg_name           = var.nsg_name
-  resource_group_name = module.resource_group.name
-  location           = module.resource_group.location
-  security_rules = [
-    {
-      name                       = "Allow-HTTPS"
-      priority                   = 100
-      direction                  = "Inbound"
-      access                     = "Allow"
-      protocol                   = "Tcp"
-      source_port_range          = "*"
-      destination_port_range     = "443"
-      source_address_prefix      = "*"
-      destination_address_prefix = "*"
+  peerings = {
+    peertovnet1 = {
+      name                                  = "${module.naming.virtual_network_peering.name_unique}-vnet2-to-vnet1"
+      remote_virtual_network_resource_id    = module.vnet1.resource_id
+      allow_forwarded_traffic               = true
+      allow_gateway_transit                 = true
+      allow_virtual_network_access          = true
+      do_not_verify_remote_gateways         = false
+      enable_only_ipv6_peering              = false
+      use_remote_gateways                   = false
+      create_reverse_peering                = true
+      reverse_name                          = "${module.naming.virtual_network_peering.name_unique}-vnet1-to-vnet2"
+      reverse_allow_forwarded_traffic       = false
+      reverse_allow_gateway_transit         = false
+      reverse_allow_virtual_network_access  = true
+      reverse_do_not_verify_remote_gateways = false
+      reverse_enable_only_ipv6_peering      = false
+      reverse_use_remote_gateways           = false
     }
-  ]
-}
-
-# Spoke-to-Spoke Peering (With Approvals)
-module "vnet_peering" {
-  source  = "Azure/avm-vnet-peering/azurerm"
-  version = "1.0.0"
-
-  local_vnet_name  = module.network.vnet_name
-  remote_vnet_id   = var.remote_vnet_id
-  allow_forwarded_traffic = true
-  allow_virtual_network_access = true
-}
-
-# Security Center
-module "security_center" {
-  source  = "Azure/avm-security-center/azurerm"
-  version = "1.0.0"
-
-  security_pricing = {
-    tier = "Standard"
   }
-}
-
-# Monitoring for Security
-module "monitoring" {
-  source  = "Azure/avm-monitor/azurerm"
-  version = "1.0.0"
-
-  resource_id               = module.firewall.id
-  log_analytics_workspace_id = var.log_analytics_workspace_id
-
-  logs = [
-    {
-      category = "FirewallRule"
-      enabled  = true
-    }
-  ]
 }
