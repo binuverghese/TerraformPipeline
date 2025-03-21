aaa5205b68c1a19dfce2be1ad7e333973b9d5de1
binuverghese <92746182+binuverghese@users.noreply.github.com>
2025-03-21 18:24:55 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,116 +1,63 @@
-variable "resource_group_name" {
-  description = "The name of the resource group."
-  type        = string
-}
-
 variable "location" {
-  description = "The Azure region where resources will be created."
   type        = string
+  description = "Azure region where resources will be deployed"
 }
 
-variable "name" {
-  description = "The name of the virtual network."
+variable "resource_group_name" {
   type        = string
+  description = "Name of the resource group"
 }
 
-variable "address_space" {
-  description = "The address space for the virtual network."
+variable "route_table_name" {
+  type        = string
+  description = "Name of the route table"
+}
+
+variable "nsg_name" {
+  type        = string
+  description = "Name of the network security group"
+}
+
+variable "vnet_name" {
+  type        = string
+  description = "Name of the virtual network"
+}
+
+variable "address_space_vnet1" {
   type        = list(string)
+  description = "Address space for the virtual network"
 }
 
 variable "dns_servers" {
-  description = "The DNS servers for the virtual network."
   type        = list(string)
-  default     = []
-}
-
-variable "ddos_protection_plan" {
-  description = "DDoS protection plan configuration."
-  type = object({
-    id     = string
-    enable = bool
-  })
-  default = null
+  description = "List of DNS servers"
 }
 
 variable "enable_vm_protection" {
-  description = "Enable VM protection."
   type        = bool
-  default     = false
+  description = "Enable VM protection"
 }
 
 variable "encryption" {
-  description = "Encryption settings for the virtual network."
-  type = object({
-    enabled     = bool
-    enforcement = string
-  })
-  default = null
+  type        = bool
+  description = "Enable encryption"
 }
 
-variable "flow_timeout_in_minutes" {
-  description = "Flow timeout setting in minutes."
-  type        = number
-  default     = 30
+variable "subnet_name" {
+  type        = string
+  description = "Subnet name"
 }
 
-variable "subnets" {
-  description = "Configuration for the subnets."
-  type = map(object({
-    name                            = string
-    address_prefixes                = list(string)
-    default_outbound_access_enabled = bool
-    delegation = list(object({
-      name               = string
-      service_delegation = object({ name = string })
-    }))
-    nat_gateway = object({ id = string })
-    network_security_group = object({ id = string })
-    route_table = object({ id = string })
-    service_endpoints        = list(string)
-    service_endpoint_policies = map(object({ id = string }))
-    role_assignments = map(object({
-      principal_id               = string
-      role_definition_id_or_name = string
-    }))
-  }))
-}
-
-variable "diagnostic_settings" {
-  description = "Diagnostic settings for monitoring."
-  type = map(object({
-    name                           = string
-    workspace_resource_id          = string
-    log_analytics_destination_type = string
-  }))
-  default = {}
-}
-
-variable "peerings" {
-  description = "Configuration for virtual network peerings."
-  type = map(object({
-    name                                  = string
-    remote_virtual_network_resource_id    = string
-    allow_forwarded_traffic               = bool
-    allow_gateway_transit                 = bool
-    allow_virtual_network_access          = bool
-    do_not_verify_remote_gateways         = bool
-    enable_only_ipv6_peering              = bool
-    use_remote_gateways                   = bool
-    create_reverse_peering                = bool
-    reverse_name                          = string
-    reverse_allow_forwarded_traffic       = bool
-    reverse_allow_gateway_transit         = bool
-    reverse_allow_virtual_network_access  = bool
-    reverse_do_not_verify_remote_gateways = bool
-    reverse_enable_only_ipv6_peering      = bool
-    reverse_use_remote_gateways           = bool
-  }))
-  default = {}
-}
-variable "address_space_vnet1" {
-  description = "The address space for VNet1"
+variable "subnet_address_prefixes" {
   type        = list(string)
+  description = "Subnet address prefixes"
 }
 
-
+variable "routes" {
+  type = list(object({
+    name           = string
+    address_prefix = string
+    next_hop_ip    = string
+  }))
+  description = "List of routes for the route table"
+}
